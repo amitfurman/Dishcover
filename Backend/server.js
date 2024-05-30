ec503c0561232b776eaf6c04ad70ec255bdcbb29
Eden-Ismah-Moshe <eden12012@gmail.com>
2024-05-31 00:18:57 +0300

    add sign in Checking user data when logging in with the DB

--- Backend/server.js
+++ Backend/server.js
@@ -8,8 +8,10 @@ const mongoose = require("mongoose");
 require("./UserDetails");
 const User = mongoose.model("UserInfo");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const mongoUrl = process.env.MONGODB_URI;
+const JWET_SECRET = process.env.JWT_SECRET;
 
 mongoose
   .connect(mongoUrl)
@@ -55,6 +57,29 @@ app.post("/signup", async (req, res) => {
     res.send({ status: "ok", data: "User created successfully" });
   } catch (e) {
     res.send({ status: "error", data: e.message });
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  const { id, password } = req.body;
+  const oldUser = await User.findOne({ $or: [{ email: id }, { name: id }] });
+
+  if (!oldUser) {
+    return res.send({ status: "error", data: "User doesn't exists." });
+  }
+
+  if (await bcrypt.compare(password, oldUser.password)) {
+    const token = jwt.sign(
+      { id: oldUser._id, email: oldUser.email },
+      JWET_SECRET
+    );
+    if (res.status(201)) {
+      return res.send({ status: "ok", data: token });
+    } else {
+      return res.send({ status: "error", data: "Error" });
+    }
+  } else {
+    return res.send({ status: "error", data: "Invalid Password" });
   }
 });
 
--- Frontend/screens/SigninScreen.js
+++ Frontend/screens/SigninScreen.js
@@ -1,53 +1,90 @@
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+  Alert,
+} from "react-native";
+import { React, useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer, useNavigation } from "@react-navigation/native"; // Import useNavigation hook
+import axios from "axios";
 
 export default function SigninScreen() {
   const navigation = useNavigation(); // useNavigation hook here
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignin() {
+    const userData = {
+      id: id,
+      password: password,
+    };
+
+    if (id.length != 0 && password.length != 0) {
+      axios.post("http://10.100.102.4:3000/signin", userData).then((res) => {
+        if (res.data.status === "ok") {
+          Alert.alert("User logged in successfully");
+          ////////////////add navigation to the next screen
+        } else {
+          Alert.alert(res.data.data);
+        }
+      });
+    } else {
+      Alert.alert("Please fill all the mandatory details");
+    }
+  }
 
   return (
-    <View className="h-full w-full flex justify-around pt-40 pb-10">
-      <View className="flex items-center justify-center">
-        <Text className="text-2xl font-bold trackimg-wider text-orange-300 mb-2">
-          Welcome Back!
-        </Text>
-        <Text className="text-xl font-light trackimg-wider text-black-200">
-          We're so excited to see you again!
-        </Text>
-      </View>
-      {/* form */}
-      <View className="flex items-center mx-4 space-y-4">
-        <View className="bg-black/5 p-5 rounded-2xl w-full mb-3 flex-row">
-          <TextInput
-            placeholder="Email or username "
-            placeholderTextColor={"gray"}
-            className="flex-1"
-          />
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+      <View className="h-full w-full flex justify-around pt-40 pb-10">
+        <View className="flex items-center justify-center">
+          <Text className="text-2xl font-bold trackimg-wider text-orange-300 mb-2">
+            Welcome Back!
+          </Text>
+          <Text className="text-xl font-light trackimg-wider text-black-200">
+            We're so excited to see you again!
+          </Text>
         </View>
-        <View className="bg-black/5 p-5 rounded-2xl w-full flex-row">
-          <TextInput
-            placeholder="Password"
-            placeholderTextColor={"gray"}
-            secureTextEntry
-            className="flex-1"
-          />
-        </View>
-        <TouchableOpacity className="self-start mb-4">
-          <Text className="text-orange-300 text-left ">Forgot Password?</Text>
-        </TouchableOpacity>
-        <View className="w-full">
-          <TouchableOpacity className="w-full p-3 rounded-2xl mb-3 bg-orange-300">
-            <Text className="text-xl font-bold text-center">Sign In</Text>
+        {/* form */}
+        <View className="flex items-center mx-4 space-y-4">
+          <View className="bg-black/5 p-5 rounded-2xl w-full mb-3 flex-row">
+            <TextInput
+              placeholder="Email or username "
+              placeholderTextColor={"gray"}
+              className="flex-1"
+              onChange={(e) => setId(e.nativeEvent.text)}
+            />
+          </View>
+          <View className="bg-black/5 p-5 rounded-2xl w-full flex-row">
+            <TextInput
+              placeholder="Password"
+              placeholderTextColor={"gray"}
+              secureTextEntry
+              className="flex-1"
+              onChange={(e) => setPassword(e.nativeEvent.text)}
+            />
+          </View>
+          <TouchableOpacity className="self-start mb-4">
+            <Text className="text-orange-300 text-left ">Forgot Password?</Text>
           </TouchableOpacity>
-        </View>
-        <View className="flex-row justify-center">
-          <Text className="text-gray-500">Don't have an account?</Text>
-          <TouchableOpacity onPress={() => navigation.navigate("Signup")}>
-            <Text className="text-orange-300"> Sign Up</Text>
-          </TouchableOpacity>
+          <View className="w-full">
+            <TouchableOpacity
+              className="w-full p-3 rounded-2xl mb-3 bg-orange-300"
+              onPress={() => handleSignin()}
+            >
+              <Text className="text-xl font-bold text-center">Sign In</Text>
+            </TouchableOpacity>
+          </View>
+          <View className="flex-row justify-center">
+            <Text className="text-gray-500">Don't have an account?</Text>
+            <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+              <Text className="text-orange-300"> Sign Up</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
-    </View>
+    </ScrollView>
   );
 }
